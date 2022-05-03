ed7eef2da3eb4d6b17609e1d9b3bcff5accd5a17
Leonardo Borges <laion459@gmail.com>
2022-05-03 09:14:15 -0300

    coloquei nomes da prof e do colega

--- src/Game.java
+++ src/Game.java
@@ -424,7 +424,17 @@ public class Game {
     }
 
     public static void main(String[] args) {
-        Player player = new Player("Vinicios, É Leonardo");
+
+        System.out.println("##################################################################################");
+        System.out.println("#   - Universidade do Vale do Itajaí UNIVALI - KOB 03/05/2022       ");
+        System.out.println("#   - PROFESSORA:       FERNANDA DOS DSNTOS CUNHA                   ");
+        System.out.println("#   - BACHARELADO:      CIÊNCIA DA COMPUTAÇÃO PROGRAMAÇÃO ORIENTADA A OBJETOS ");
+        System.out.println("#   - MATERIA:          PROGRAMAÇÃO ORIENTADA A OBJETOS ");
+        System.out.println("#   - ALUNOS:           VINICIUS MATEUS COLET , LEONARDO DARIO BORGES");
+        System.out.println("##################################################################################");
+        System.out.println();
+
+        Player player = new Player("Vinicius, É Leonardo");
         System.out.println();
         player.callGame();
 
