0cc9ea98d7070b1dd4df4a5b8b16bad75c9a83aa
Leonardo Borges <laion459@gmail.com>
2022-06-28 09:56:26 -0300

    criação Main

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("##################################################################################");
+        System.out.println("#   - Universidade do Vale do Itajaí UNIVALI - KOB 03/05/2022       ");
+        System.out.println("#   - PROFESSORA:       FERNANDA DOS DSNTOS CUNHA                   ");
+        System.out.println("#   - BACHARELADO:      CIÊNCIA DA COMPUTAÇÃO PROGRAMAÇÃO ORIENTADA A OBJETOS ");
+        System.out.println("#   - MATERIA:          PROGRAMAÇÃO ORIENTADA A OBJETOS ");
+        System.out.println("#   - ALUNOS:           VINICIUS MATEUS COLET , LEONARDO DARIO BORGES");
+        System.out.println("##################################################################################");
+        System.out.println();
+
+        Player player = new Player("Leonardo");
+        System.out.println();
+        player.callGame();
+
+    }
+}
