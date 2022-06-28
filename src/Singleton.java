165cba8a7d8d44d1262eb3edf01abe825ec8b61d
Leonardo Borges <laion459@gmail.com>
2022-06-28 16:30:17 -0300

    texto explicativo na classe Singleton

--- src/Singleton.java
+++ src/Singleton.java
@@ -2,4 +2,8 @@ public class Singleton {
     // 1- Construtor privado
     // 2- Método acesso público ex: getInstance
     // 3- Atributo estático de classe Singleton
+
+
+    // **********  ATENÇÃO!!  **********
+    // Foi implementado na classe WORDS o modelo de padrão de progeto!!
 }
